63b90bcce8eeb584b7453dd090ce193abcab049b
luis <luisf@hexasoftware.com>
2017-05-25 23:06:07 -0300

    Logger uses handlerFunc now

--- handler_log.go
+++ handler_log.go
@@ -19,20 +19,20 @@ func (l *logHelper) WriteHeader(code int) {
 	l.ResponseWriter.WriteHeader(code)
 }
 
-func LogHandler(name string, next http.Handler) http.Handler {
+func LogHandler(name string, next http.HandlerFunc) http.HandlerFunc {
 	//llog := log.New(os.Stderr, "["+name+"]: ", 0)
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		l := &logHelper{w, 200}
 		if next != nil {
 			next.ServeHTTP(l, r)
 		}
 		log.Printf("[%s] %s (%d) - %s", name, r.Method, l.statusCode, r.URL.Path)
-	})
+	}
 }
 
 //Logger middleware for logging handlerFunc
 func ChainLogger(name string) ChainFunc {
 	return func(next http.Handler) http.Handler {
-		return LogHandler(name, next)
+		return LogHandler(name, next.ServeHTTP)
 	}
 }
